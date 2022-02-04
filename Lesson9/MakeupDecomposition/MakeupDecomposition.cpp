1c79f35d7c97eb91dc952de3c305b625334c08d7
Terabiterr <brolerbrat@gmail.com>
2022-02-04 11:12:30 +0200

    lesson9 add makeup

---
+++ Lesson9/MakeupDecomposition/MakeupDecomposition.cpp
@@ -0,0 +1,35 @@
+﻿#include <iostream>
+#include <vector>
+#include <string>
+#include <map>
+using namespace std;
+
+// Декомпозиция auto
+// Сверстка
+// C++ 17
+
+template <typename T, typename ... Ts>
+void insert_all(vector<T>& vec, Ts ... ts)
+{
+    (vec.push_back(ts), ...);
+}
+int main()
+{
+    // Декомпозиция
+    vector<int> v{ 1, 2, 3 };
+    insert_all(v, 4, 5, 6);
+
+    for (auto& data : v)
+        cout << data << endl;
+    // Сверстка
+    map<int, int> m{
+        {1, v[0]},
+        {2, v[1]},
+        {3, v[2]},
+        {4, v[3]},
+        {5, v[4]},
+        {6, v[5]},
+    };
+    for (auto& [key, value] : m)
+        cout << "key:\t" << key << "\t" << "value:\t" << value << endl;
+}
